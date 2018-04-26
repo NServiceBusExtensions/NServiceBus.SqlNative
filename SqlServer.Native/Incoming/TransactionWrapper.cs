aba2c70a1dcca0751156e9410063a71cd75807c0
Simon Cropp <simon.cropp@gmail.com>
2018-04-26 22:21:10 +1000

    .

---
+++ SqlServer.Native/Incoming/TransactionWrapper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+static class TransactionWrapper
+{
+    public static async Task<T> Run<T>(SqlConnection connection, SqlTransaction transaction, Func<SqlTransaction, Task<T>> func)
+    {
+        var ownsTransaction = false;
+        if (transaction == null)
+        {
+            ownsTransaction = true;
+            transaction = connection.BeginTransaction();
+        }
+
+        try
+        {
+            var incomingResult = await func(transaction).ConfigureAwait(false);
+            if (ownsTransaction)
+            {
+                transaction.Commit();
+            }
+
+            return incomingResult;
+        }
+        catch (Exception)
+        {
+            if (ownsTransaction)
+            {
+                transaction.Rollback();
+            }
+
+            throw;
+        }
+        finally
+        {
+            if (ownsTransaction)
+            {
+                transaction.Dispose();
+            }
+        }
+    }
+}
