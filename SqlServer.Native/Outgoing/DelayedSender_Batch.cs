d0a9b9ae67d904ea3e5d34d2db51b8706f6d4c01
Simon Cropp <simon.cropp@gmail.com>
2018-04-26 22:17:55 +1000

    .

--- SqlServer.Native/Outgoing/DelayedSender_Batch.cs
+++ SqlServer.Native/Outgoing/DelayedSender_Batch.cs
@@ -9,80 +9,58 @@ namespace NServiceBus.Transport.SqlServerNative
 {
     public partial class DelayedSender
     {
-        public virtual async Task Send(string connection, IEnumerable<OutgoingDelayedMessage> messages, CancellationToken cancellation = default)
+        public virtual async Task<long> Send(string connection, IEnumerable<OutgoingDelayedMessage> messages, CancellationToken cancellation = default)
         {
             Guard.AgainstNullOrEmpty(connection, nameof(connection));
             Guard.AgainstNull(messages, nameof(messages));
             using (var sqlConnection = new SqlConnection(connection))
             {
                 await sqlConnection.OpenAsync(cancellation).ConfigureAwait(false);
-                await InnerSend(sqlConnection, null, messages, cancellation).ConfigureAwait(false);
+                return await InnerSend(sqlConnection, null, messages, cancellation).ConfigureAwait(false);
             }
         }
 
-        public virtual Task Send(SqlConnection connection, IEnumerable<OutgoingDelayedMessage> messages, CancellationToken cancellation = default)
+        public virtual Task<long> Send(SqlConnection connection, IEnumerable<OutgoingDelayedMessage> messages, CancellationToken cancellation = default)
         {
             Guard.AgainstNull(connection, nameof(connection));
             Guard.AgainstNull(messages, nameof(messages));
             return InnerSend(connection, null, messages, cancellation);
         }
 
-        public virtual Task Send(SqlTransaction transaction, IEnumerable<OutgoingDelayedMessage> messages, CancellationToken cancellation = default)
+        public virtual Task<long> Send(SqlTransaction transaction, IEnumerable<OutgoingDelayedMessage> messages, CancellationToken cancellation = default)
         {
             Guard.AgainstNull(transaction, nameof(transaction));
             Guard.AgainstNull(messages, nameof(messages));
             return InnerSend(transaction.Connection, transaction, messages, cancellation);
         }
 
-        async Task InnerSend(SqlConnection connection, SqlTransaction transaction, IEnumerable<OutgoingDelayedMessage> messages, CancellationToken cancellation)
+        Task<long> InnerSend(SqlConnection connection, SqlTransaction transaction, IEnumerable<OutgoingDelayedMessage> messages, CancellationToken cancellation)
         {
-            var ownsTransaction = false;
-            if (transaction == null)
+            return TransactionWrapper.Run(connection, transaction, sqlTransaction => NewMethod(sqlTransaction, messages, cancellation));
+        }
+
+        async Task<long> NewMethod(SqlTransaction transaction, IEnumerable<OutgoingDelayedMessage> messages, CancellationToken cancellation)
+        {
+            long rowVersion = 0;
+            using (var command = transaction.Connection.CreateCommand())
             {
-                ownsTransaction = true;
-                transaction = connection.BeginTransaction();
+                command.Transaction = transaction;
+                var parameters = command.Parameters;
+                command.CommandText = string.Format(Sql, table);
+                var dueParam = parameters.Add("Due", SqlDbType.DateTime);
+                var headersParam = parameters.Add("Headers", SqlDbType.NVarChar);
+                var bodyParam = parameters.Add("Body", SqlDbType.VarBinary);
+                foreach (var message in messages)
+                {
+                    cancellation.ThrowIfCancellationRequested();
+                    dueParam.Value = message.Due;
+                    headersParam.Value = message.Headers;
+                    bodyParam.SetValueOrDbNull(message.Body);
+                    rowVersion = (long) await command.ExecuteScalarAsync(cancellation).ConfigureAwait(false);
+                }
             }
 
-            try
-            {
-                using (var command = connection.CreateCommand())
-                {
-                    command.Transaction = transaction;
-                    var parameters = command.Parameters;
-                    command.CommandText = string.Format(Sql, table);
-                    var dueParam = parameters.Add("Due", SqlDbType.DateTime);
-                    var headersParam = parameters.Add("Headers", SqlDbType.NVarChar);
-                    var bodyParam = parameters.Add("Body", SqlDbType.VarBinary);
-                    foreach (var message in messages)
-                    {
-                        cancellation.ThrowIfCancellationRequested();
-                        dueParam.Value = message.Due;
-                        headersParam.Value = message.Headers;
-                        bodyParam.SetValueOrDbNull(message.Body);
-                        await command.ExecuteNonQueryAsync(cancellation).ConfigureAwait(false);
-                    }
-                }
-                if (ownsTransaction)
-                {
-                    transaction.Commit();
-                }
-            }
-            catch (Exception)
-            {
-                if (ownsTransaction)
-                {
-                    transaction.Rollback();
-                }
-
-                throw;
-            }
-            finally
-            {
-                if (ownsTransaction)
-                {
-                    transaction.Dispose();
-                }
-            }
+            return rowVersion;
         }
     }
 }
