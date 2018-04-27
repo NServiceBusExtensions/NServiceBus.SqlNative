7453b44d31f204d33e54c83ce51eabd470d3c7da
Simon Cropp <simon.cropp@gmail.com>
2018-04-27 11:18:03 +1000

    .

---
+++ SqlServer.Native/BodyComputedColumnBuilder.cs
@@ -0,0 +1,14 @@
+﻿static class BodyComputedColumnBuilder
+{
+    public static string Computed(string computedColumnSql)
+    {
+        if (computedColumnSql == null)
+        {
+            return @"
+    BodyString as cast(Body as varchar(max)),";
+        }
+
+        return $@"
+    BodyString as {computedColumnSql},";
+    }
+}
