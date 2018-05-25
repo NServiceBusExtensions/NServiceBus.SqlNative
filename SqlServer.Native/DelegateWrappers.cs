be7780f09095654e176940013bc79cd65ca9bb49
Simon Cropp <simon.cropp@gmail.com>
2018-05-25 21:20:52 +1000

    .

---
+++ SqlServer.Native/DelegateWrappers.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Threading.Tasks;
+
+static class DelegateWrappers
+{
+    static string threwAnException = "Provided {0} delegate threw an exception.";
+    static string returnedNull = "Provided {0} delegate returned a null.";
+
+    public static Func<T, K> WrapFunc<T, K>(this Func<T, K> func, string name)
+    {
+        var exceptionMessage = string.Format(threwAnException, name);
+        var nullMessage = string.Format(returnedNull, name);
+        return x =>
+        {
+            K value;
+            try
+            {
+                value = func(x);
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exceptionMessage, exception);
+            }
+
+            if (value == null)
+            {
+                throw new Exception(nullMessage);
+            }
+
+            return value;
+        };
+    }
+
+    public static Action<T> WrapFunc<T>(this Action<T> func, string name)
+    {
+        var exceptionMessage = string.Format(threwAnException, name);
+        return x =>
+        {
+            try
+            {
+                func(x);
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exceptionMessage, exception);
+            }
+        };
+    }
+
+    public static Action<T, K> WrapFunc<T, K>(this Action<T, K> func, string name)
+    {
+        var exceptionMessage = string.Format(threwAnException, name);
+        return (x, y) =>
+        {
+            try
+            {
+                func(x, y);
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exceptionMessage, exception);
+            }
+        };
+    }
+
+    public static Func<T1, T2, T3, Task> WrapFunc<T1, T2, T3>(this Func<T1, T2, T3, Task> func, string name)
+    {
+        var exceptionMessage = string.Format(threwAnException, name);
+        var nullMessage = string.Format(returnedNull, name);
+        return async (x, y, z) =>
+        {
+            Task task;
+            try
+            {
+                task = func(x, y, z);
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exceptionMessage, exception);
+            }
+
+            if (task == null)
+            {
+                throw new Exception(nullMessage);
+            }
+
+            try
+            {
+                await task.ConfigureAwait(false);
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exceptionMessage, exception);
+            }
+        };
+    }
+
+    public static Func<T, Task<K>> WrapFunc<T, K>(this Func<T, Task<K>> func, string name)
+    {
+        var exceptionMessage = string.Format(threwAnException, name);
+        var nullMessage = string.Format(returnedNull, name);
+        return async x =>
+        {
+            Task<K> task;
+            try
+            {
+                task = func(x);
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exceptionMessage, exception);
+            }
+
+            if (task == null)
+            {
+                throw new Exception(nullMessage);
+            }
+
+            try
+            {
+                return await task.ConfigureAwait(false);
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exceptionMessage, exception);
+            }
+        };
+    }
+}
--- SqlServer.Native/Guard.cs
+++ SqlServer.Native/Guard.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Data;
 using System.Data.SqlClient;
-using System.Threading.Tasks;
 
 static class Guard
 {
@@ -26,14 +25,6 @@ static class Guard
             throw new ArgumentException("Connection must be open.", argumentName);
         }
     }
-
-    //public static void AgainstSqlDelimiters(string argumentName, string value)
-    //{
-    //    if (value.Contains("]") || value.Contains("[") || value.Contains("`"))
-    //    {
-    //        throw new ArgumentException($"The argument '{value}' contains a ']', '[' or '`'. Names and schemas automatically quoted.");
-    //    }
-    //}
 
     public static void AgainstNullOrEmpty(string value, string argumentName)
     {
@@ -92,126 +83,5 @@ static class Guard
         {
             throw new ArgumentNullException(argumentName);
         }
-    }
-
-    public static Func<T, K> WrapFunc<T, K>(this Func<T, K> func, string name)
-    {
-        var exceptionMessage = $"Provided {name} delegate threw an exception.";
-        var nullMessage = $"Provided {name} delegate returned a null.";
-        return x =>
-        {
-            K value;
-            try
-            {
-                value = func(x);
-            }
-            catch (Exception exception)
-            {
-                throw new Exception(exceptionMessage, exception);
-            }
-
-            if (value == null)
-            {
-                throw new Exception(nullMessage);
-            }
-
-            return value;
-        };
-    }
-
-    public static Action<T> WrapFunc<T>(this Action<T> func, string name)
-    {
-        var exceptionMessage = $"Provided {name} delegate threw an exception.";
-        return x =>
-        {
-            try
-            {
-                func(x);
-            }
-            catch (Exception exception)
-            {
-                throw new Exception(exceptionMessage, exception);
-            }
-        };
-    }
-
-    public static Action<T, K> WrapFunc<T, K>(this Action<T, K> func, string name)
-    {
-        var exceptionMessage = $"Provided {name} delegate threw an exception.";
-        return (x, y) =>
-        {
-            try
-            {
-                func(x, y);
-            }
-            catch (Exception exception)
-            {
-                throw new Exception(exceptionMessage, exception);
-            }
-        };
-    }
-
-    public static Func<T1, T2, T3, Task> WrapFunc<T1, T2, T3>(this Func<T1, T2, T3, Task> func, string name)
-    {
-        var exceptionMessage = $"Provided {name} delegate threw an exception.";
-        var nullMessage = $"Provided {name} delegate returned a null.";
-        return async (x, y, z) =>
-        {
-            Task task;
-            try
-            {
-                task = func(x, y, z);
-            }
-            catch (Exception exception)
-            {
-                throw new Exception(exceptionMessage, exception);
-            }
-
-            if (task == null)
-            {
-                throw new Exception(nullMessage);
-            }
-
-            try
-            {
-                await task.ConfigureAwait(false);
-            }
-            catch (Exception exception)
-            {
-                throw new Exception(exceptionMessage, exception);
-            }
-        };
-    }
-
-    public static Func<T, Task<K>> WrapFunc<T, K>(this Func<T, Task<K>> func, string name)
-    {
-        var exceptionMessage = $"Provided {name} delegate threw an exception.";
-        var nullMessage = $"Provided {name} delegate returned a null.";
-        return async x =>
-        {
-            Task<K> task;
-            try
-            {
-                task = func(x);
-            }
-            catch (Exception exception)
-            {
-                throw new Exception(exceptionMessage, exception);
-            }
-
-            if (task == null)
-            {
-                throw new Exception(nullMessage);
-            }
-
-            try
-            {
-                return await task.ConfigureAwait(false);
-            }
-            catch (Exception exception)
-            {
-                throw new Exception(exceptionMessage, exception);
-            }
-        };
     }
 }
