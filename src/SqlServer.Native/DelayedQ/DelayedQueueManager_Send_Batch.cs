9345dcd2959e789bfc43436b9944305c80f8cb12
Simon Cropp <simon.cropp@gmail.com>
2020-09-30 21:18:14 +1000

    Async batch (#382)

---
+++ src/SqlServer.Native/DelayedQ/DelayedQueueManager_Send_Batch.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NServiceBus.Transport.SqlServerNative
+{
+    public partial class DelayedQueueManager
+    {
+        public virtual async Task Send(IEnumerable<OutgoingDelayedMessage> messages, CancellationToken cancellation = default)
+        {
+            Guard.AgainstNull(messages, nameof(messages));
+            using var command = Connection.CreateCommand(Transaction, string.Format(SendSql, Table));
+            var dueParameter = CreateDueParameter(command);
+            var headersParameter = CreateHeadersParameter(command);
+            var bodyParameter = CreateBodyParameter(command);
+
+            foreach (var message in messages)
+            {
+                dueParameter.Value = message.Due;
+                headersParameter.Value = message.Headers;
+                bodyParameter.SetBinaryOrDbNull(message.Body);
+                await command.RunNonQuery(cancellation);
+            }
+        }
+
+        public virtual async Task Send(IAsyncEnumerable<OutgoingDelayedMessage> messages, CancellationToken cancellation = default)
+        {
+            Guard.AgainstNull(messages, nameof(messages));
+            using var command = Connection.CreateCommand(Transaction, string.Format(SendSql, Table));
+            var dueParameter = CreateDueParameter(command);
+            var headersParameter = CreateHeadersParameter(command);
+            var bodyParameter = CreateBodyParameter(command);
+
+            await foreach (var message in messages.WithCancellation(cancellation))
+            {
+                dueParameter.Value = message.Due;
+                headersParameter.Value = message.Headers;
+                bodyParameter.SetBinaryOrDbNull(message.Body);
+                await command.RunNonQuery(cancellation);
+            }
+        }
+    }
+}
---
+++ src/SqlServer.Native/MainQ/QueueManager_Send_Batch.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NServiceBus.Transport.SqlServerNative
+{
+    public partial class QueueManager
+    {
+        public virtual async Task Send(IEnumerable<OutgoingMessage> messages, CancellationToken cancellation = default)
+        {
+            Guard.AgainstNull(messages, nameof(messages));
+            using var command = Connection.CreateCommand(Transaction, string.Format(sendSql, Table));
+            var parameters = command.Parameters;
+
+            var idParameter = CreateIdParameter(command, parameters);
+            var expiresParameter = CreateExpiresParameter(command, parameters);
+            var headersParameter = CreateHeadersParameter(command, parameters);
+            var bodyParameter = CreateBodyParameter(command, parameters);
+            foreach (var message in messages)
+            {
+                idParameter.Value = message.Id;
+                expiresParameter.SetValueOrDbNull(message.Expires);
+                headersParameter.Value = message.Headers;
+                bodyParameter.SetBinaryOrDbNull(message.Body);
+                await command.RunNonQuery(cancellation);
+            }
+        }
+
+        public virtual async Task Send(IAsyncEnumerable<OutgoingMessage> messages, CancellationToken cancellation = default)
+        {
+            Guard.AgainstNull(messages, nameof(messages));
+            using var command = Connection.CreateCommand(Transaction, string.Format(sendSql, Table));
+            var parameters = command.Parameters;
+
+            var idParameter = CreateIdParameter(command, parameters);
+            var expiresParameter = CreateExpiresParameter(command, parameters);
+            var headersParameter = CreateHeadersParameter(command, parameters);
+            var bodyParameter = CreateBodyParameter(command, parameters);
+            await foreach (var message in messages.WithCancellation(cancellation))
+            {
+                idParameter.Value = message.Id;
+                expiresParameter.SetValueOrDbNull(message.Expires);
+                headersParameter.Value = message.Headers;
+                bodyParameter.SetBinaryOrDbNull(message.Body);
+                await command.RunNonQuery(cancellation);
+            }
+        }
+    }
+}
