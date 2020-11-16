3cd46c11bdd2606971a54146937f46315f56514b
Greg Bielleman <greg.bielleman@gmail.com>
2020-11-17 08:46:42 +1100

    Change queue create to migrate Index_RowVersion to a non-clustered index (#424)

--- src/SqlServer.Native.Tests/MainQ/MainQueueCreationTests.cs
+++ src/SqlServer.Native.Tests/MainQ/MainQueueCreationTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 using NServiceBus.Transport.SqlServerNative;
 using VerifyTests;
 using VerifyXunit;
@@ -17,5 +18,38 @@ public class MainQueueCreationTests
         var settings = new VerifySettings();
         settings.SchemaSettings(includeItem: s => s == "MainQueueCreationTests");
         await Verifier.Verify(connection, settings);
+    }
+
+    [Fact]
+    public async Task MigrateToNonClusteredIndex()
+    {
+        await using var connection = Connection.OpenConnectionFromNewClient();
+        var tableName = "MigrateToNonClusteredIndexTest";
+
+        var manager = new QueueManager(tableName, connection);
+        await manager.Drop();
+        await manager.Create();
+
+        //Simulate old version by dropping the Index_RowVersion and replace it with clustered index
+        var makeOldIndexCommand = new SqlCommand($@"
+drop index Index_RowVersion on {tableName};
+create clustered index Index_RowVersion on {tableName}
+(
+	RowVersion
+)
+", connection);
+        await makeOldIndexCommand.ExecuteNonQueryAsync();
+
+        //Rerun Create should drop the clustered index and create a nonclustered index
+        await manager.Create();
+        var settings = new VerifySettings();
+
+         var getIndexTypeDesCommand = new SqlCommand($@"select type_desc from sys.indexes
+where object_id = object_id('{tableName}')
+and name = 'Index_RowVersion'",
+             connection);
+
+
+         await Verifier.Verify(await getIndexTypeDesCommand.ExecuteScalarAsync() as string, settings);
     }
 }
