12042c8639a7889bb322300dd23f52230948e06b
Greg Bielleman <Greg.Bielleman@me.com>
2020-11-16 17:31:16 +1100

    Change queue create to migrate Index_RowVersion to a non-clustered index

--- src/SqlServer.Native/MainQ/QueueManager_QueueCreator.cs
+++ src/SqlServer.Native/MainQ/QueueManager_QueueCreator.cs
@@ -9,39 +9,47 @@
         /// The sql statements used to create the queue.
         /// </summary>
         public override string CreateTableSql => @"
-if exists (
-  select *
-  from sys.objects
-  where object_id = object_id('{0}')
-    and type in ('U'))
-return
+if not exists (select * from sys.objects  where object_id = object_id('{0}') and type = 'U')
+    create table {0} (
+	    Id uniqueidentifier not null,
+	    CorrelationId varchar(255),
+	    ReplyToAddress varchar(255),
+	    Recoverable bit not null,
+	    Expires datetime,
+	    Headers nvarchar(max) not null,{1}
+	    Body varbinary(max),
+	    RowVersion bigint identity(1,1) not null);
 
-create table {0} (
-  Id uniqueidentifier not null,
-  CorrelationId varchar(255),
-  ReplyToAddress varchar(255),
-  Recoverable bit not null,
-  Expires datetime,
-  Headers nvarchar(max) not null,{1}
-  Body varbinary(max),
-  RowVersion bigint identity(1,1) not null
-);
+-- drop any existing clustered index called Index_RowVersion
+if exists (select * from sys.indexes
+	where object_id = object_id('{0}')
+	and name = 'Index_RowVersion'
+	and type = 1)
 
-create clustered index Index_RowVersion on {0}
+drop index Index_RowVersion on {0}
+
+-- create nonclustered index Index_RowVersion if it doesn't exist
+if not exists (select * from sys.indexes
+	where object_id = object_id('{0}')
+	and name = 'Index_RowVersion'
+	and type = 2)
+create nonclustered index Index_RowVersion on {0}
 (
-  RowVersion
+	RowVersion
 )
 
+if not exists (select * from sys.indexes
+                where object_id = object_id('{0}')
+                and name = 'Index_Expires')
 create nonclustered index Index_Expires on {0}
 (
-  Expires
+	Expires
 )
 include
 (
-  Id,
-  RowVersion
+	Id,
+	RowVersion
 )
-where
-  Expires is not null";
+where Expires is not null";
     }
 }
