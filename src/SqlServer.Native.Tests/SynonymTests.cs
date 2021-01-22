22e162716327ef2b480617fabb78bed89caaa9e8
Simon Cropp <simon.cropp@gmail.com>
2021-01-22 12:24:28 +1100

    add Synonym

---
+++ src/SqlServer.Native.Tests/SynonymTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using NServiceBus.Transport.SqlServerNative;
+using Xunit;
+
+public class SynonymTests :
+    TestBase
+{
+    [Fact]
+    public async Task CreateDrop()
+    {
+        var subscriptionManager = new Synonym(SqlConnection, SqlConnection.Database);
+        await using (var command = SqlConnection.CreateCommand())
+        {
+            command.CommandText = "create table target(id int);";
+            try
+            {
+                await command.ExecuteNonQueryAsync();
+            }
+            catch (SqlException)
+            {
+            }
+        }
+
+        await subscriptionManager.Drop("mySynonym");
+        await subscriptionManager.Create("mySynonym", "target");
+    }
+}
---
+++ src/SqlServer.Native/Synonym.cs
@@ -0,0 +1,64 @@
+﻿using System.Data.Common;
+using System.Threading.Tasks;
+
+namespace NServiceBus.Transport.SqlServerNative
+{
+    public class Synonym
+    {
+        DbConnection connection;
+        string targetDatabase;
+        DbTransaction? transaction;
+
+        public Synonym(DbConnection connection, string targetDatabase)
+        {
+            Guard.AgainstNull(connection, nameof(connection));
+            Guard.AgainstNullOrEmpty(targetDatabase, nameof(targetDatabase));
+            this.connection = connection;
+            this.targetDatabase = targetDatabase;
+        }
+
+        public Synonym(DbTransaction transaction, string targetDatabase)
+        {
+            Guard.AgainstNull(transaction, nameof(transaction));
+            Guard.AgainstNullOrEmpty(targetDatabase, nameof(targetDatabase));
+            this.transaction = transaction;
+            this.targetDatabase = targetDatabase;
+            connection = transaction.Connection;
+        }
+
+        public async Task Create(string synonym, string target)
+        {
+            using var command = connection.CreateCommand();
+            command.Transaction = transaction;
+            command.CommandText = $@"
+if not exists (
+    select 0
+    from sys.synonyms
+    where [name]=N'{synonym}'
+)
+begin
+    create synonym [{synonym}]
+    for {targetDatabase}.[{target}];
+end
+";
+            await command.ExecuteNonQueryAsync();
+        }
+
+        public async Task Drop(string synonym)
+        {
+            using var command = connection.CreateCommand();
+            command.Transaction = transaction;
+            command.CommandText = $@"
+if exists (
+    select 0
+    from sys.synonyms
+    where [name]=N'{synonym}'
+)
+begin
+    drop synonym [{synonym}];
+end
+";
+            await command.ExecuteNonQueryAsync();
+        }
+    }
+}
