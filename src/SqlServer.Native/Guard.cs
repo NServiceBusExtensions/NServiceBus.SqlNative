55ec313417f1632af3434a702fcf5770262fe6e4
Simon Cropp <simon.cropp@gmail.com>
2023-03-13 22:30:43 +1100

    move to polyfill

--- src/SqlServer.Native/Guard.cs
+++ src/SqlServer.Native/Guard.cs
@@ -58,16 +58,3 @@
         }
     }
 }
-#if NET48
-namespace System.Runtime.CompilerServices
-{
-    [AttributeUsage(AttributeTargets.Parameter)]
-    sealed class CallerArgumentExpressionAttribute : Attribute
-    {
-        public CallerArgumentExpressionAttribute(string parameterName) =>
-            ParameterName = parameterName;
-
-        public string ParameterName { get; }
-    }
-}
-#endif
