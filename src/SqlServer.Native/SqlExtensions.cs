74d3b2aaadecd5282ca97b8cd2f86fba07fe6123
Simon Cropp <simon.cropp@gmail.com>
2023-12-11 11:38:46 +1100

    Update SqlExtensions.cs

--- src/SqlServer.Native/SqlExtensions.cs
+++ src/SqlServer.Native/SqlExtensions.cs
@@ -2,26 +2,12 @@
 
 static class SqlExtensions
 {
-    static string quoteSuffix = "]";
-    static string quotePrefix = "[";
-
     public static string Sanitize(string unquotedIdentifier)
     {
         var builder = new StringBuilder();
-        if (!string.IsNullOrEmpty(quotePrefix))
-        {
-            builder.Append(quotePrefix);
-        }
-
-        if (string.IsNullOrEmpty(quoteSuffix))
-        {
-            builder.Append(unquotedIdentifier);
-        }
-        else
-        {
-            builder.Append(unquotedIdentifier.Replace(quoteSuffix, quoteSuffix + quoteSuffix));
-            builder.Append(quoteSuffix);
-        }
+        builder.Append('[');
+        builder.Append(unquotedIdentifier.Replace("]", "]]"));
+        builder.Append(']');
 
         return builder.ToString();
     }
