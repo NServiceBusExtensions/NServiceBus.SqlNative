1218f85cfd87f8e7163521526b628d8385abe657
Simon Cropp <simon.cropp@gmail.com>
2024-04-02 22:38:31 +1100

    refs and cleanup

--- src/SqlServer.Native.Tests/GlobalUsings.cs
+++ src/SqlServer.Native.Tests/GlobalUsings.cs
@@ -1,6 +1,4 @@
-﻿// Global using directives
-
-global using System.Security.Claims;
+﻿global using System.Security.Claims;
 global using Microsoft.Data.SqlClient;
 global using Microsoft.Extensions.DependencyInjection;
 global using NServiceBus.SqlServer.HttpPassthrough;
